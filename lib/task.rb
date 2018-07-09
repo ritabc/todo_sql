72c78496ca90b47893b59e7566c46a8006a53e85
Annie Shin and Rita Bennett-Chew <rita@>
2018-07-09 09:58:44 -0700

    initial commit and pass test 1

---
+++ lib/task.rb
@@ -0,0 +1,21 @@
+class Task
+  attr_reader(:description)
+
+  def initialize(attributes)
+   @description = attributes.fetch(:description)
+  end
+
+  def self.all
+   returned_tasks = DB.exec("SELECT * FROM tasks;")
+   tasks = []
+   returned_tasks.each() do |task|
+     description = task.fetch("description")
+     tasks.push(Task.new({:description => description}))
+   end
+   tasks
+  end
+
+  def ==(another_task)
+    self.description().==(another_task.description())
+  end
+end
---
+++ spec/task_spec.rb
@@ -0,0 +1,21 @@
+require("rspec")
+require("pg")
+require("task")
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
+
+describe(Task) do
+  describe('#==') do
+    it('is the same task if it has the same description') do
+      task1 = Task.new({:description => 'learn SQL'})
+      task2 = Task.new({:description => 'learn SQL'})
+      expect(task1).to(eq(task2))
+    end
+  end
+end
